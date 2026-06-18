d50dc343f4ce69622f9673076405e496ba502fe4
Samuel Rounce <me@samuelrounce.co.uk>
2026-06-18 08:46:29 +0100

    Unpack VM762 V1.4 driver

---
+++ Linux/Kali-23.2-linux-headers-6.1.0.sh
@@ -0,0 +1,8 @@
+wget -c https://old.kali.org/kali/pool/main/l/linux/linux-headers-6.1.0-kali9-common_6.1.27-1kali1_all.deb
+dpkg -i linux-headers-6.1.0-kali9-common_6.1.27-1kali1_all.deb
+wget -c http://old.kali.org/kali/pool/main/l/linux/linux-kbuild-6.1_6.1.27-1kali1_amd64.deb
+dpkg -i linux-kbuild-6.1_6.1.27-1kali1_amd64.deb
+wget -c http://old.kali.org/kali/pool/main/l/linux/linux-compiler-gcc-12-x86_6.1.27-1kali1_amd64.deb
+dpkg -i linux-compiler-gcc-12-x86_6.1.27-1kali1_amd64.deb
+wget -c http://old.kali.org/kali/pool/main/l/linux/linux-headers-6.1.0-kali9-amd64_6.1.27-1kali1_amd64.deb
+dpkg -i linux-headers-6.1.0-kali9-amd64_6.1.27-1kali1_amd64.deb
---
+++ Linux/Kali-24.2-linux-headers-6.6.15.sh
@@ -0,0 +1,6 @@
+wget -c https://old.kali.org/kali/pool/main/l/linux/linux-headers-6.6.15-common_6.6.15-2kali1_all.deb
+dpkg -i linux-headers-6.6.15-common_6.6.15-2kali1_all.deb
+wget -c https://old.kali.org/kali/pool/main/l/linux/linux-kbuild-6.6.15_6.6.15-2kali1%2Bb1_amd64.deb
+dpkg -i linux-kbuild-6.6.15_6.6.15-2kali1+b1_amd64.deb
+wget -c https://old.kali.org/kali/pool/main/l/linux/linux-headers-6.6.15-amd64_6.6.15-2kali1%2Bb1_amd64.deb
+dpkg -i linux-headers-6.6.15-amd64_6.6.15-2kali1+b1_amd64.deb
